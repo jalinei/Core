5d62a39f5c4ab03b0c5515152c0b800954bcadef
Jean Alinei <jean.alinei@laas.fr>
2024-02-21 18:04:44 +0100

    Supressed calls to env that are not yet available in after_install.py
    Signed-off-by: Jean Alinei <jean.alinei@laas.fr>

--- scripts/after_install.py
+++ scripts/after_install.py
@@ -1,14 +1,14 @@
 import os
 import shutil
 
-env = DefaultEnvironment()
-
 zephyr_path = os.path.join(".", "zephyr")
+print(zephyr_path)
 owntech_path = os.path.join(".", "owntech")
+print(owntech_path)
 if not os.path.isdir(owntech_path):
     os.mkdir(owntech_path)
 
-lib_path = os.path.join(".", "owntech", "lib", env.Dump("PIOENV").strip("''"), "core")
+lib_path = os.path.join(".", "lib", "owntech_core")
 print(lib_path)
 
 shutil.move(os.path.join(lib_path, "zephyr"), zephyr_path)
